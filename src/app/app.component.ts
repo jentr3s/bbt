f7dd33a87286b3c1586f683cef23fb2d9f418e3d
jentr3s <jentr3s03@gmail.com>
2018-11-19 00:02:24 +0800

    finat bbt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bbt';
+
+  data: any = {
+  };
+
+  message: string = '';
+
+  meetings: any = [
+    { id: 1, name: 'one' },
+    { id: 2, name: 'two' },
+  ];
+
+  days: any = [
+    { id: 1, name: "Monday" },
+    { id: 2, name: "Tuesday" },
+    { id: 3, name: "Wednesday" },
+    { id: 4, name: "Thursday" },
+    { id: 5, name: "Friday" },
+    { id: 6, name: "Saturday" },
+    { id: 7, name: "Sunday" },
+  ]
+
+  onSubmit() {
+    alert(JSON.stringify(this.data));
+  }
 }
